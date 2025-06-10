8ab6dd9a3b9c9a40f48f93dd163d5564c238feef
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-06-10 11:52:38 +0200

    fix: 🔒 Update environment variable for Django secret key

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
