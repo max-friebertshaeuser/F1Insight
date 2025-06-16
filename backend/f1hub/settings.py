f7227dd6743523e07a5d6122f3666e31a1947e06
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-06-16 07:04:02 +0200

    feat: ✨ Extend ACCESS_TOKEN_LIFETIME to 7 days in settings.py

--- backend/f1hub/settings.py
+++ backend/f1hub/settings.py
@@ -156,6 +156,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
